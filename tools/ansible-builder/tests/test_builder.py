dd16247d81250d067de03bc60d696385cb4a29a7
Daniel Goldman <danielgoldman4@gmail.com>
2022-02-05 15:21:42 -0500

    add a test

---
+++ tools/ansible-builder/tests/test_builder.py
@@ -0,0 +1,9 @@
+from alpacloud_ansible_builder import collect_mappings
+
+
+def test_find_multiple_collections():
+	collections = [{'a': {}, 'b': {}}, {'c': {}}]
+
+	a = collect_mappings(collections)
+
+	assert len(a) == 3
