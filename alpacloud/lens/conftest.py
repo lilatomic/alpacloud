63af09a2704b8f04ba0d3fed84cb5b115eac35af
Daniel Goldman <danielgoldman4@gmail.com>
2025-06-25 17:23:43 -0400

    add testcases for easy recorded tests

--- alpacloud/lens/conftest.py
+++ alpacloud/lens/conftest.py
@@ -1,4 +1,37 @@
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Callable
+
+import yaml
+
+
+@dataclass
+class Case:
+	a: Any
+	b: Any
+
+	def __call__(self, f: Callable):
+		assert f(self.a) == self.b
+
+
+@dataclass
+class ResourceLoader:
+	base_path: Path
+
+	def load_obj(self, name: str):
+		return self._load_file(self.base_path / name)
+
+	def _load_file(self, p: Path):
+		return yaml.safe_load(p.open())
+
+	def load_case(self, path, name: str):
+		def l(e):
+			return self._load_file(self.base_path / path / f"{name}.{e}.yml")
+
+		return Case(l("a"), l("b"))
+
+	def __getitem__(self, item):
+		return self.load_obj(item + ".yml")
 
 
 @dataclass
