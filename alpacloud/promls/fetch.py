06b420355cbf75895051c06d6cfd999cc55734be
Daniel Goldman <danielgoldman4@gmail.com>
2026-01-04 14:31:51 -0500

    refactor: rebuild metrics parser

--- alpacloud/promls/fetch.py
+++ alpacloud/promls/fetch.py
@@ -31,17 +31,98 @@ class FetcherURL:
 class ParseError(Exception):
 	"""Error parsing Prometheus metrics endpoint."""
 
-	def __init__(self, value, line: str):
+	def __init__(self, value, line: str, cursor: int | None = None):
 		self.line = line
+		self.cursor = cursor
 		super().__init__(value)
 
 	def __str__(self) -> str:
-		return super().__str__() + f" line={self.line}"
+		msg = super().__str__() + f" line={self.line}"
+		if self.cursor is not None:
+			msg += f" cursor={self.cursor}"
+		return msg
+
+
+whitespace = re.compile(r"\s+")
+name = re.compile(r"[a-zA-Z_][a-zA-Z0-9_]*")
+
+
+class LineReader:
+	def __init__(self, line: str):
+		self.line = line
+		self.cursor = 0
+
+	def err(self, msg: str) -> None:
+		raise ParseError(msg, self.line, self.cursor)
+
+	def peek(self):
+		return self.line[self.cursor]
+
+	def peek_for(self, char: str) -> bool:
+		return self.peek() == char
+
+	def consume_for(self, char: str) -> str:
+		if self.peek_for(char):
+			self.cursor += 1
+			return char
+		return ""
+
+	def restore(self, cursor: int):
+		self.cursor = cursor
+
+	def consume_whitespace(self) -> bool:
+		match = whitespace.match(self.line, self.cursor)
+		if match:
+			self.cursor = match.end()
+			return True
+		return False
+
+	def read_name(self):
+		match = name.match(self.line, self.cursor)
+		if match:
+			self.cursor = match.end()
+			return match.group()
+		return None
+
+	def read_escaped(self, until: str):
+		out = ""
+		while self.line[self.cursor] != until:
+			# TODO: can optimise to add in slices until escaped char is reached
+			if self.line[self.cursor] == "\\":
+				# TODO: ensure valid escape sequences
+				char_at = self.cursor + 1
+				self.cursor += 2
+				out += self.line[char_at]
+			else:
+				char_at = self.cursor
+				self.cursor += 1
+				out += self.line[char_at]
+
+			if self.cursor >= len(self.line):
+				self.err("Unterminated string literal")
+		self.cursor += 1  # TODO: use consume_for?
+		return out
+
+	def read_label_value(self):
+		return self.read_escaped('"')
+
+	def read_value(self):
+		"""Read a value from the line. Value must be a valid float, or NaN or Inf."""
+		start = self.cursor
+		end = start
+		while len(self.line) > self.cursor and self.line[self.cursor] != " ":
+			self.cursor += 1
+			end = self.cursor
+		try:
+			return float(self.line[start:end])
+		except ValueError:
+			self.err("Invalid numeric value")
+
+	def read_remaining(self):
+		return self.line[self.cursor :]
 
 
 class Parser:
-	"""Parse metrics from Prometheus metrics endpoint."""
-
 	@dataclass
 	class DataLine:
 		"""Data line from Prometheus metrics endpoint."""
@@ -66,86 +147,91 @@ class Parser:
 		kind: Parser.MetaKind
 		data: str
 
-	def parse(self, lines: list[str]) -> dict[str, Metric]:
-		"""Parse metrics from Prometheus metrics endpoint."""
-		name2data = self.group_lines(lines)
-		return {k: self.parse_metric(k, v) for k, v in name2data.items()}
-
-	def group_lines(self, lines: list[str]):
-		"""Group lines by the metric name."""
-		name2data = defaultdict(list)
-		for line in lines:
-			# escape empty lines
-			if not line.strip():
-				continue
-
-			is_data = not line.startswith("#")
-
-			d: Parser.DataLine | Parser.MetaLine
-			if is_data:
-				d = self.parse_data_line(line)
-			else:
-				d = self.parse_meta_line(line)
-
-			name2data[d.name].append(d)
-		return name2data
+	def __init__(self, r: LineReader):
+		self.r = r
 
 	@staticmethod
-	def parse_data_line(line: str) -> DataLine:
-		"""Subparser for a line of data."""
-		# TODO: handle escaping in lines
-		x = line.split(" ", 1)
-		if len(x) != 2:
-			raise ParseError("Could not identify name in line", line)
+	def assemble(lines: list[Parser.DataLine | Parser.MetaLine]):
+		"""Assemble parsed lines into metrics"""
+		# TODO: gather comments
+		meta = defaultdict(list)
+		for line in lines:
+			if isinstance(line, Parser.MetaLine):
+				meta[line.name].append(line)
 
-		name_and_labels, data = x
-		data = data.strip()
+		metrics = []
+		for line in lines:
+			if isinstance(line, Parser.DataLine):
+				metrics.append(Parser.parse_metric(line.name, [line, *meta[line.name]]))
 
-		if line.count("{") != line.count("}"):
-			raise ParseError(r"Invalid data line, unmatched `{}` pair", line)
+		return metrics
 
-		pieces = list(filter(None, re.split(r"[{}]", name_and_labels)))
-		if len(pieces) > 2:
-			# TODO: better error
-			raise ParseError(f"Invalid data line, split into incorrect number of pieces pieces: {len(pieces)}", line)
+	def p_anyline(self) -> Parser.DataLine | Parser.MetaLine | None:
+		if not self.r.line.strip():
+			return None
 
-		name = pieces[0].strip()
+		if self.r.consume_for("#"):
+			return self.p_comment()
+		else:
+			return self.p_metric()
+
+	def p_comment(self):
+		"""Parse a comment line"""
+		self.r.consume_whitespace()
+		restore_cursor = self.r.cursor
+		kind = self.r.read_name()
+		if kind == "HELP" or kind == "TYPE":
+			if not self.r.consume_whitespace():
+				self.r.err(f"Expected whitespace after comment type {kind}")
+			metric_name = self.r.read_name()
+			if metric_name is None:
+				self.r.err(f"Invalid metric name in {kind} comment")
+			self.r.consume_whitespace()
+			return Parser.MetaLine(metric_name, Parser.MetaKind(kind), self.r.read_remaining())
+		else:
+			self.r.restore(restore_cursor)
+			return Parser.MetaLine("COMMENT", Parser.MetaKind.COMMENT, self.r.read_remaining())  # TODO: model comment so we don't have an arbitrary value for `name`
+
+	def p_metric(self):
+		"""Parse a metric line"""
+		name = self.r.read_name()
+		if name is None:
+			self.r.err("Invalid metric name")
 
 		labels = {}
-		if len(pieces) > 1:
-			for label_pair in pieces[1].split(","):
-				label_key, label_value = label_pair.split("=")
-				labels[label_key.strip()] = label_value.strip('"')
+		if self.r.consume_for("{"):
+			label_name, label_value = self.p_label()
+			labels[label_name] = label_value
 
-		if " " in data:
-			# if it's a counter, it will also contain a timestamp
-			value, timestamp_raw = data.split(" ", 1)
-			timestamp = int(timestamp_raw.strip())
+			# consume all labels
+			while self.r.consume_for(","):
+				if self.r.peek_for("}"):  # We peek here because of potential trailing comma
+					break
+				label_name, label_value = self.p_label()
+				labels[label_name] = label_value
+
+			if not self.r.consume_for("}"):
+				self.r.err("Expected closing brace after labels")
+
+		if not self.r.consume_whitespace():
+			self.r.err("Expected whitespace after labels")
+		value = self.r.read_value()
+
+		if self.r.consume_whitespace():
+			timestamp = self.r.read_value()
 		else:
-			value, timestamp = data, None
+			timestamp = None
 
-		return Parser.DataLine(name, labels, value.strip(), timestamp)
+		return Parser.DataLine(name, labels, value, timestamp)
 
-	@staticmethod
-	def parse_meta_line(line: str) -> MetaLine:
-		"""Subparser for a line of metadata."""
-		if not line.startswith("#"):
-			raise ParseError(r"Invalid metadata line, did not start with #", line)
-
-		line = line.strip("#").strip()
-		maybe_kind = line.split(" ", maxsplit=1)
-		if maybe_kind[0] in Parser.MetaKind.__members__:
-			kind = Parser.MetaKind(maybe_kind[0])
-			tail = maybe_kind[1]
-		else:
-			kind = Parser.MetaKind.COMMENT
-			tail = line
-
-		if kind == Parser.MetaKind.COMMENT:
-			return Parser.MetaLine("COMMENT", kind, tail)  # TODO: model comment so we don't have an arbitrary value for `name`
-		[name, data] = tail.split(" ", maxsplit=1)
-
-		return Parser.MetaLine(name, kind, data)
+	def p_label(self):
+		name = self.r.read_name()
+		if not self.r.consume_for("="):
+			self.r.err("Expected `=` after label name")
+		if not self.r.consume_for('"'):
+			self.r.err('Expected `"` after `=`')
+		value = self.r.read_label_value()
+		return name, value
 
 	@staticmethod
 	def parse_metric(name, statements: list[Parser.DataLine | Parser.MetaLine]) -> Metric:
--- alpacloud/promls/fetch_test.py
+++ alpacloud/promls/fetch_test.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 from alpacloud.lens.conftest import ResourceLoader
-from alpacloud.promls.fetch import Parser
+from alpacloud.promls.fetch import LineReader, Parser, Parser2
+from alpacloud.promls.metrics import Metric
 
 
 class TestParserDataline:
@@ -59,18 +60,17 @@ class TestParserMetaLine:
 
 class TestParseAll:
 	def test_doc_sample(self):
-		# TODO: support for escaped values
 		# """
-		# # Escaping in label values:
-		# msdos_file_access_time_ms{path="C:\\DIR\\FILE.TXT",error="Cannot find file:\n\"FILE.TXT\""} 1.234e3
 		# # A weird metric from before the epoch:
 		# something_weird{problem="division by zero"} +Inf -3982045
 		# """
 		l = """\
+# Escaping in label values:
+msdos_file_access_time_ms{path="C:\\\\DIR\\\\FILE.TXT",error="Cannot find file:\\n\\"FILE.TXT\\""} 1.234e3
 # HELP api_http_request_count The total number of HTTP requests.
 # TYPE api_http_request_count counter
-http_request_count{method="post",code="200"} 1027 1395066363000
-http_request_count{method="post",code="400"}    3 1395066363000
+api_http_request_count{method="post",code="200"} 1027 1395066363000
+api_http_request_count{method="post",code="400"}    3 1395066363000
 # Minimalistic line:
 metric_without_timestamp_and_labels 12.47
 # Finally a summary, which has a pretty complex representation in the text format:
@@ -84,8 +84,22 @@ telemetry_requests_metrics_latency_microseconds{quantile="0.99"} 76656
 telemetry_requests_metrics_latency_microseconds_sum 1.7560473e+07
 telemetry_requests_metrics_latency_microseconds_count 2693
 		"""
-		r = Parser().parse(l.split("\n"))
-		assert len(r) == 7
+		r = [Parser2(LineReader(l)).p_anyline() for l in l.split("\n")]
+		r = list(filter(None, r))
+		r = Parser2.assemble(r)
+		assert r == [
+			Metric(name="msdos_file_access_time_ms", help="", type=""),
+			Metric(name="api_http_request_count", help="The total number of HTTP requests.", type="counter"),
+			Metric(name="api_http_request_count", help="The total number of HTTP requests.", type="counter"),
+			Metric(name="metric_without_timestamp_and_labels", help="", type=""),
+			Metric(name="telemetry_requests_metrics_latency_microseconds", help="A histogram of the response latency.", type="summary"),
+			Metric(name="telemetry_requests_metrics_latency_microseconds", help="A histogram of the response latency.", type="summary"),
+			Metric(name="telemetry_requests_metrics_latency_microseconds", help="A histogram of the response latency.", type="summary"),
+			Metric(name="telemetry_requests_metrics_latency_microseconds", help="A histogram of the response latency.", type="summary"),
+			Metric(name="telemetry_requests_metrics_latency_microseconds", help="A histogram of the response latency.", type="summary"),
+			Metric(name="telemetry_requests_metrics_latency_microseconds_sum", help="", type=""),
+			Metric(name="telemetry_requests_metrics_latency_microseconds_count", help="", type=""),
+		]
 
 	def test_certmanager_sample(self):
 		r = ResourceLoader(Path(__file__).parent / "test_resources").load_raw("certmanager.prom")
